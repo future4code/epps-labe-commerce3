adb2c6bac7d3d52b163b30b61113d2eaa6a8001d
cptvictor95 <cpt.victor@hotmail.com>
2021-01-06 19:37:55 -0300

    corrigida responsivididade do filtro

--- src/components/Produto.js
+++ src/components/Produto.js
@@ -8,9 +8,25 @@ const Text = styled.p`
 const Li = styled.li`
   list-style: none;
   border-bottom: 1px solid #bbbbbb;
+  display: flex;
+  justify-content: space-around;
 `;
 
-const Button = styled.button``;
+const Button = styled.button`
+  padding: 0.3rem 0.7rem;
+  border-radius: 5px;
+  border: none;
+  cursor: pointer;
+  background-color: tomato;
+  margin: 0.5rem;
+  font-weight: 900;
+  font-size: 1rem;
+  color: #fff;
+
+  &:hover {
+    background-color: #e65036;
+  }
+`;
 
 class Produto extends Component {
   render() {
